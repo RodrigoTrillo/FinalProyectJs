217c5506e4b366ebcbfb2fc555c90b01293bb450
Rodrigo Trillo <rollyt1232@gmail.com>
2022-10-13 19:21:48 -0300

    add form

--- app.js
+++ app.js
@@ -6,15 +6,32 @@ const botonVaciar = document.getElementById("vacias-carrito")
 
 const contadorCarrito = document.getElementById("contadorCarrito")
 
-const fragment = document.createDocumentFragment()
+const finCompra = document.querySelector("#finCompra")
 
 const precioTotal = document.getElementById("precioTotal")
 
 let carrito =[]
 
+const formulario = document.getElementById("formulario")
+const inputs = document.querySelectorAll("#formulario input")
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+const campos = {
+	nombre: false,
+	correo: false,
+	telefono: false
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     localStorage.getItem('carrito')? (carrito = JSON.parse(localStorage.getItem('carrito')), actualizarCarrito()): null
 })
+
+
 
 botonVaciar.addEventListener(`click`, ()=>{
     carrito=[];
@@ -73,10 +90,6 @@ let producto= fetch("/data.json")
         const item = carrito.find((prod)=> prod.id === prodId)
         const indice = carrito.indexOf(item)
         carrito.splice(indice, 1)
-        if(carrito.length = 0){
-            carrito = [];
-        }
-        localStorage.clear()
         actualizarCarrito()
     }
 
@@ -98,14 +111,76 @@ let producto= fetch("/data.json")
             localStorage.setItem("carrito", JSON.stringify(carrito))
         })
         contadorCarrito.innerHTML= carrito.reduce((acc, prod)=> acc + prod.cantidad, 0)
-        //contadorCarrito.innerHTML=carrito.length
         precioTotal.innerText = carrito.reduce((acc, prod)=> acc + prod.precio*prod.cantidad, 0)
     }
 
 
 
 
+// codigo para el formulario
 
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+		case "telefono":
+			validarCampo(expresiones.telefono, e.target, 'telefono');
+		break;
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const terminos = document.getElementById('terminos');
+	if(campos.nombre && campos.correo && campos.telefono && terminos.checked ){
+		formulario.reset();
+        carrito=[];
+        localStorage.clear();
+        actualizarCarrito();
+
+
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
+});
 
 
 
